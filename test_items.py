bfb79d4e4393fd76993dda2fa559f1b885f6b5ab
Daniil Petrashov <danechkapetrashov@yandex.ru>
2026-09-18 19:19:29 +0300

    change test function name

--- test_items.py
+++ test_items.py
@@ -5,10 +5,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_auth_button_is_displayed(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
-    auth_button = WebDriverWait(browser, 20).until(EC.element_to_be_clickable(
+    button = WebDriverWait(browser, 20).until(EC.element_to_be_clickable(
         (By.CLASS_NAME, "btn-add-to-basket")))
 
 
-    assert auth_button.is_displayed(), "Onboarding action button isn't displayed"
+    assert button.is_displayed(), "Onboarding action button isn't displayed"
